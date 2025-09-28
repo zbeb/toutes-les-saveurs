8bee3b7a5e1a1abe8387ab5668b1e4b94d26e5ff
zbeb Dev <71894094+zbeb@users.noreply.github.com>
2025-09-29 01:08:52 +0200

    Add files via upload

---
+++ src/js/model.js
@@ -0,0 +1,66 @@
+import { API_URL } from "./config.js";
+import { getJSON } from "./helpers.js";
+import { RES_PER_PAGE } from "./config.js";
+
+export const state = {
+    recipe: {},
+    search: {
+        query: '',
+        results: [],
+        resultsPerPage: RES_PER_PAGE,
+        page: 1,
+    },
+};
+
+export const loadRecipe = async function(id) {
+    try {
+        const data = await getJSON(`${API_URL}${id}`)
+
+        const {recipe} = data.data;
+        state.recipe = {
+            id: recipe.id,
+            title: recipe.title,
+            publisher: recipe.publisher,
+            sourceUrl: recipe.source_url,
+            imageUrl: recipe.image_url,
+            servings: recipe.servings,
+            cookingTime: recipe.cooking_time,
+            ingredients: recipe.ingredients
+        }
+        console.log(state.recipe);
+    }
+    catch (err) {
+        console.log(`${err} ✈🏢💥`);
+        throw err;
+    }
+}
+
+export const loadSearchResults = async function(query) {
+    try {
+        state.search.query = query;
+        const data = await getJSON(`${API_URL}?search=${query}`);
+
+        state.search.results = data.data.recipes.map(rec => {
+            return {
+                id: rec.id,
+                title: rec.title,
+                publisher: rec.publisher,
+                sourceUrl: rec.source_url,
+                imageUrl: rec.image_url
+            }
+        });
+    }
+    catch (err) {
+        console.log(`${err} ✈🏢💥`);
+        throw err;
+    }
+}
+
+export const getSearchResultsPage = function(page = state.search.page) {
+    state.search.page = page;
+
+    const start = (page - 1) * state.search.resultsPerPage; // 0;
+    const end = page * state.search.resultsPerPage; // 9;
+
+    return state.search.results.slice(start, end);
+}
